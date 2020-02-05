9c0d2df4dad0782a4b456edbba8e76b95d207bf7
Lorenzo Agnolucci <lory.agnolucci@gmail.com>
2020-02-05 16:51:06 +0100

    First implementation of Bolt to parse tweets

---
+++ src/main/java/TweetParserBolt.java
@@ -0,0 +1,44 @@
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+
+import org.apache.storm.tuple.Values;
+import twitter4j.Status;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class TweetParserBolt extends BaseRichBolt{
+
+    private OutputCollector collector;
+    String[] keywords;
+
+    public TweetParserBolt(String[] keywords){
+        this.keywords = keywords;
+    }
+
+    @Override
+    public void prepare(Map<String, Object> map, TopologyContext topologyContext, OutputCollector outputCollector){
+        this.collector = outputCollector;
+    }
+
+    @Override
+    public void execute(Tuple tuple){
+        Status tweet = (Status) tuple.getValueByField("tweet");
+        ArrayList<String> tweetKeywords = new ArrayList<String>();
+        for(String keyword : keywords){
+            if(tweet.getText().contains(keyword)){
+                tweetKeywords.add(keyword);
+            }
+        }
+        collector.emit(new Values(tweet.getId(), tweet.getText(), tweetKeywords));
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer){
+        outputFieldsDeclarer.declare(new Fields("tweet_ID", "text", "keywords"));
+    }
+}
